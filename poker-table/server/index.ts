45d7b733568f948877f5a05092ca0d9c41bc24d1
andrefiker <andrefiker@gmail.com>
2026-09-11 15:36:53 -0300

    poker: let room server own active-table ticking

--- poker-table/server/index.ts
+++ poker-table/server/index.ts
@@ -1,6 +1,3 @@
-import server, { poker } from './app.js';
+import server from './app.js';
 
-await poker.tick();
-const timer = setInterval(() => { void poker.tick(); }, 1_000);
-timer.unref();
 server.listen(Number(process.env.PORT ?? 3000), () => console.log('poker server listening'));
